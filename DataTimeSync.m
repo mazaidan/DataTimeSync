df53cad910c177d536d22edd8671422660b89bb1
zaidanma <martha.zaidan@helsinki.fi>
2018-03-19 18:52:23 +0200

    Update functions become two nargins only

--- DataTimeSync.m
+++ DataTimeSync.m
@@ -1,4 +1,4 @@
-function DataOutputFinal = DataTimeSync(data_input_only,time_input_datevec,time_output_datevec,varargin)
+function DataOutputFinal = DataTimeSync(DataInput,time_output_datevec,varargin)
 
 % DataTimeSync is the function used to syncronize the time from the data
 % downloaded from https://avaa.tdata.fi/web/smart
@@ -18,18 +18,22 @@ function DataOutputFinal = DataTimeSync(data_input_only,time_input_datevec,time_
 % Institute for Atmospheric and Earth System Research (INAR)/Physics
 % Helsinki University, Finland
 
-if nargin < 3 || nargin > 5
+if nargin < 2 || nargin > 4
     error('The number of input argument is incorrect');
-elseif nargin == 3
+elseif nargin == 2
     time_type = 1;         % default
     method    = 'mean'; % default
-elseif nargin == 5
+elseif nargin == 3
     time_type = varargin{1};
     method    = 'mean'; % default
-elseif nargin ==5
+elseif nargin ==4
     time_type = varargin{1};
-    method    = varargin{2}; % 'mean', 'median'    
+    method    = varargin{2}; % 'mean', N/A  
 end
+
+time_input_datevec=DataInput(:,1:6); 
+data_input_only=DataInput(:,7:end);
+
 
 % Input time_output:
 datetime_input = datetime(time_input_datevec,'InputFormat','dd-MMM-yyyy HH:mm:ss');
--- DataTimeSync_testing.m
+++ DataTimeSync_testing.m
@@ -4,6 +4,11 @@ clear;close all; clc
 % Specify input time_output and data:
 time_input_datenum = datenum({'01-Jan-2015 00:00:00';'31-Jan-2015 23:30:00'});
 time_input_datevec = datevec(time_input_datenum(1):1/480:time_input_datenum(2));
+
+% if we use the below case, we have to apply interpolation
+% time_input_datevec = datevec(time_input_datenum(1):1/40:time_input_datenum(2));
+% because the input is less data than output
+
 data_input_only = randn(size(time_input_datevec,1),1);
 
 data_input_only(1,1)=NaN;
@@ -11,9 +16,15 @@ data_input_only(5,1)=NaN;
 % Specify output time_output:
 time_output_datevec = datevec(time_input_datenum(1):1/48:time_input_datenum(2));
 
-%DataOutputFinal = DataTimeSync(data_input_only,time_input_datevec,time_output_datevec);
-DataOutputFinal = DataTimeSync(data_input_only,time_input_datevec,time_output_datevec,1,'median');
+DataInput=[time_input_datevec data_input_only data_input_only];
+
+
+DataOutputFinal = DataTimeSync(DataInput,time_output_datevec);
+%DataOutputFinal = DataTimeSync(DataInput,time_output_datevec,1,'mean');
+
 
 % check the result:
 nanmean(data_input_only(1:10,1))
 DataOutputFinal(1,end)
+
+clear time_input_datenum time_input_datevec data_input_only 
